8dd58b2ca241b7f76cf246a7002e7721c89f3443
Maku18 <jose18_carl@hotmail.com>
2018-04-22 12:26:13 -0500

    The profile page now loads user's name and description, test file and jsp working

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -10,10 +10,38 @@ import javax.servlet.http.HttpServletRequest;
 
 import javax.servlet.http.HttpServletResponse;
 
+import codeu.model.data.Conversation;
+import codeu.model.data.Message;
+import codeu.model.data.User;
+import codeu.model.store.basic.ConversationStore;
+import codeu.model.store.basic.MessageStore;
+import codeu.model.store.basic.UserStore;
 
 
 public class ProfileServlet extends HttpServlet {
+  private UserStore userStore;
+  private MessageStore messageStore;
+  private ConversationStore conversationStore;
 
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    setUserStore(UserStore.getInstance());
+    setMessageStore(MessageStore.getInstance());
+    setConversationStore(ConversationStore.getInstance());
+  }
+
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
+  }
+
+  void setMessageStore(MessageStore messageStore){
+    this.messageStore = messageStore;
+  }
+
+  void setConversationStore(ConversationStore conversationStore){
+    this.conversationStore = conversationStore;
+  }
 
   @Override
 
@@ -21,7 +49,59 @@ public class ProfileServlet extends HttpServlet {
 
     throws IOException, ServletException {
 
+    String requestUrl = request.getRequestURI();
+    String userName = requestUrl.substring("/user/".length());
+  
+    User user = userStore.getUser(userName);
+    request.setAttribute("user", user);
     request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
   }
+
+  /*@Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    String username = (String) request.getSession().getAttribute("user");
+    if (username == null) {
+      // user is not logged in, don't let them add a message
+      response.sendRedirect("/login");
+      return;
+    }
+
+    User user = userStore.getUser(username);
+    if (user == null) {
+      // user was not found, don't let them add a message
+      response.sendRedirect("/login");
+      return;
+    }
+
+    String requestUrl = request.getRequestURI();
+    String conversationTitle = requestUrl.substring("/chat/".length());
+
+    Conversation conversation = conversationStore.getConversationWithTitle(conversationTitle);
+    if (conversation == null) {
+      // couldn't find conversation, redirect to conversation list
+      response.sendRedirect("/conversations");
+      return;
+    }
+
+    String messageContent = request.getParameter("message");
+
+    // this removes any HTML from the message content
+    String cleanedMessageContent = Jsoup.clean(messageContent, Whitelist.none());
+
+    Message message =
+        new Message(
+            UUID.randomUUID(),
+            conversation.getId(),
+            user.getId(),
+            cleanedMessageContent,
+            Instant.now());
+
+    messageStore.addMessage(message);
+
+    // redirect to a GET request
+    response.sendRedirect("/chat/" + conversationTitle);
+  }*/
 
 }
--- src/main/webapp/WEB-INF/view/profile.jsp
+++ src/main/webapp/WEB-INF/view/profile.jsp
@@ -1,18 +1,48 @@
+<%@ page import="codeu.model.data.User" %>
+<%@ page import="codeu.model.store.basic.UserStore" %>
+<%@ page import="java.util.List" %>
+<%@ page import="codeu.model.data.Conversation" %>
+<%@ page import="codeu.model.data.Message" %>
+
+<%User userProfile = (User) request.getAttribute("user");
+//List<Message> messages = (List<Message>) request.getAttribute("messages");
+%>
 <!DOCTYPE html>
-
 <html>
-
 <head>
+ <title> Profile <%=userProfile.getName()%></title>
 
- <title>Register</title>
+ <!--          Style        -->
+ <link rel="stylesheet" href="/css/main.css" type="text/css">
 
+  <style>
+    #chat {
+      background-color: white;
+      height: 500px;
+      overflow-y: scroll
+    }
+  </style>
+  <!-- ...................... -->
 </head>
 
 <body>
+  <%@ include file="/nav.jsp" %>
 
- <h1>Profile</h1>
+ <h1><%=userProfile.getName()%>'s profile</h1>
+ <p><b>About <%=userProfile.getName()%></b></p>
+ <p><%=userProfile.getDescription()%></p>
 
- <p>Our JSP is working!</p>
+ <!-- <%//if (request.getSession().getAttribute("user") != null) { %>
+  <%//if(((User)request.getSession().getAttribute("user")).getName().equals(userProfile.getName())){ %>
+    <h1>Own Profile</h1> 
+  <%//} else { %> 
+    <h1>Other Profile</h1>
+  <%//}%>
+ <%//} else { %>
+    <h1>Other Profile</h1>
+ <%//} %> -->
+
+ 
 
 </body>
 
